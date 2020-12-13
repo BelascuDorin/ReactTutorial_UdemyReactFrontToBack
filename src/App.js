71d5a1493a45bc96c62090f1b762a850621bd495
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-12-13 16:35:52 +0200

    Added constrains to the props

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 class App extends Component{
 
   render(){
+    const numbers = [1, 2, 3, 4];
+
     return (
       <div className='App'>
-        <Navbar ></Navbar>
+        <Navbar title={numbers}/>
       </div>
     );  
   }
