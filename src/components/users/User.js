7dc597d409aa4fd222448ae89723605aea443f59
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-12-19 20:43:26 +0200

    Attached context to: User, Search, Repos

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,13 @@
-import React, { Fragment, useEffect} from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import Repos from '../repos/Repos'
+import GithubContext from '../../context/github/githubContext';
 
+const  User = ({ match }) =>  {
+    const githubContext = useContext(GithubContext);
 
-const  User = ({ user, loading, getUser, getUserRepos, repos, match}) =>  {
+    const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
     useEffect(() => {
         getUser(match.params.login);
@@ -14,6 +16,7 @@ const  User = ({ user, loading, getUser, getUserRepos, repos, match}) =>  {
 
         // eslint-disable-next-line  
     }, []); // [] -> enumerate the properties it should run for when they change
+
 
     const {
         name,
@@ -86,14 +89,6 @@ const  User = ({ user, loading, getUser, getUserRepos, repos, match}) =>  {
         </Fragment>
     )
     
-}
-
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired,
 }
 
 export default User
